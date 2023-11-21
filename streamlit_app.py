093fe336becf3d0ee74e1eb0441f636329cf81f2
jurajkret221439 <112891104+jurajkret221439@users.noreply.github.com>
2023-11-21 23:10:05 +0100

    changes2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import pandas as pd
 import streamlit as st
 
 
-st.header("st.button")
 
-if st.button("Say Hello"):
-    st.write("Why Hello there")
-else:
-    st.write("Goodbye")
+add_sidebar= st.sidebar.selectbox("shit", ("fuck", "bitch", "ass"))
+
+if add_sidebar == "fuck":
+    st.title("mrdka")
+    st.header("houno")
+    st.write("klobasa")
