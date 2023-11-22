74a3bc81530662acb3bcb0b868dc33e8f98052c6
jurajkret221439 <112891104+jurajkret221439@users.noreply.github.com>
2023-11-22 17:25:40 +0100

    added dish info to database

--- setup_database.py
+++ setup_database.py
@@ -77,3 +77,43 @@ add_dish_ingredient(taco_beef, birria, 80)
 add_dish_ingredient(taco_beef, avo_smash, 25)
 add_dish_ingredient(taco_beef, onion_coriander, 25)
 add_dish_ingredient(taco_beef, mojo_rojo, 12)
+
+
+# Add Frietje Rendang and its ingredients
+frietje_rendang = add_dish("Frietje Rendang")
+rendang = add_ingredient("Rendang")
+fries = add_ingredient("Fries")
+danish_cheese = add_ingredient("Danish Cheese")
+zz_kk = add_ingredient("SS Cucumber") 
+
+add_dish_ingredient(frietje_rendang, rendang, 120)
+add_dish_ingredient(frietje_rendang, fries, 80)
+add_dish_ingredient(frietje_rendang, danish_cheese, 50)
+add_dish_ingredient(frietje_rendang, zz_kk, 25)
+
+
+# Add Taco Shrimp and its ingredients
+taco_shrimp = add_dish("Taco Shrimp")
+shrimp = add_ingredient("Shrimp")
+salsa_verde = add_ingredient("Salsa Verde")
+pico_de_gallo = add_ingredient("Pico de Gallo")
+iceberg = add_ingredient("Iceberg Lettuce")
+tempura = add_ingredient("Tempura Batter")
+
+add_dish_ingredient(taco_shrimp, shrimp, 2) # fix it to be pieces
+add_dish_ingredient(taco_shrimp, salsa_verde, 30)
+add_dish_ingredient(taco_shrimp, pico_de_gallo, 40)
+add_dish_ingredient(taco_shrimp, iceberg, 25)
+add_dish_ingredient(taco_shrimp, tempura, 24) # not mixed
+
+
+# Add Popcorn Shrimp and its ingredients
+popcorn_shrimp = add_dish("Popcorn Shrimp")
+yuzu_mayo = add_ingredient("Yuzu Mayonaise")
+
+
+add_dish_ingredient(popcorn_shrimp, shrimp, 5)
+add_dish_ingredient(popcorn_shrimp, yuzu_mayo, 60)
+add_dish_ingredient(popcorn_shrimp, tempura, 60) # not mixed
+
+
